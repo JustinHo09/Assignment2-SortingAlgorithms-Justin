0c7355d733d5493519b91c34d745f84fc4cddf5a
JustinHo09 <hojustin09@gmail.com>
2026-02-01 14:44:39 -0600

    Added array to store sizes, integer to store iterations, created an Tester object, and created SortingAlgorithms objects, and loops to run each one.
    I added an array sizes that will store all the sizes that will be used to test the array, and made an integer to store how many iterations we want to do for each one. Then created a Tester object which we will use to call the sorting algorithms mehtods. Then added a print statement to distinguish which sections are which algorithm. I also added loops to run test on the corresponding algorithm for each input size.

--- src/Performance.java
+++ src/Performance.java
@@ -1,2 +1,53 @@
 public class Performance {
+
+    public void main(){
+
+        int [] sizes = {100,500,1000,2000,5000,10000,20000,75000,150000};
+        int iterate = 20;
+
+        Tester testing;
+
+        SortingAlgorithm bubble = new BubbleSort();
+        SortingAlgorithm insertion = new InsertionSort();
+        SortingAlgorithm selection = new SelectionSort();
+        SortingAlgorithm shell = new ShellSort();
+
+
+        // Bubble sort
+        System.out.println("Sorting algorithm - Bubble sort");
+        testing= new Tester(bubble);
+        for(int i=0; i<sizes.length; i++){
+            testing.test(iterate,sizes[i]);
+        }
+
+        // Insertion sort
+        System.out.println("Sorting algorithm - Insertion sort");
+        testing = new Tester(insertion);
+        for(int i=0; i<sizes.length; i++){
+            testing.test(iterate,sizes[i]);
+        }
+
+        // Selection sort
+        System.out.println("Sorting algorithm - Selection sort");
+        testing = new Tester(selection);
+        for(int i=0; i<sizes.length; i++){
+            testing.test(iterate,sizes[i]);
+        }
+
+        // Shell sort
+        System.out.println("Sorting algorithm - Shell sort");
+        testing = new Tester(shell);
+        for( int i=0; i<sizes.length; i++){
+            testing.test(iterate,sizes[i]);
+        }
+
+        // Quick sort
+        System.out.println("Sorting algorithm - Quick sort");
+
+
+        // Merge sort
+        System.out.println("Sorting algorithm - Merge sort");
+
+
+    }
 }

ee891ac4c622b8df50a3e86bb69a7837cb028baf
JustinHo09 <hojustin09@gmail.com>
2026-01-31 21:49:10 -0600

    Removed temp variable from sorty.
    I removed the temp variable from sorty since it is not needed anymore now that I have a method call to sort.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,7 +1,6 @@
 public class BubbleSort implements  SortingAlgorithm{
 
     public int[] sorty(int[] input){
-        int temp;
         for(int i =0; i<input.length-1; i++){
             for(int j=0; j<input.length-1-i; j++){
                 if(input[j+1] < input[j]){

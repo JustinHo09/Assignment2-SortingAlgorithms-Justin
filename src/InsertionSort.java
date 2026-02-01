2a568757390fa7b9df2b4f2b4673d4dbe056bb5c
JustinHo09 <hojustin09@gmail.com>
2026-01-31 20:47:56 -0600

    I finished the code for insertion sort.
    I finished the sorty method to perform insertion sort on the inputed array. I did so using swaps.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,6 +1,18 @@
 public class InsertionSort implements SortingAlgorithm{
 
     public int[] sorty(int[] input){
-        
+        int temp;
+        for(int i=1; i<input.length;i++){
+
+            for(int j=i; j>0; j--){
+
+                if (input[j] < input[j-1]){
+                    temp = input[j-1];
+                    input[j-1] = input[j];
+                    input[j] = temp;
+                }
+            }
+        }
+        return input;
     }
 }

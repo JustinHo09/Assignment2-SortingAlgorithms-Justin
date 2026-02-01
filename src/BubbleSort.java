1cd16d284730b0d5c294066f9ae249636dc5fb70
JustinHo09 <hojustin09@gmail.com>
2026-01-31 20:33:35 -0600

    I added code to do bubble sort.
    I added code that does bubble sort for the bubble sort implementation of sorty.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,6 +1,16 @@
 public class BubbleSort implements  SortingAlgorithm{
 
     public int[] sorty(int[] input){
-        
+        int temp;
+        for(int i =0; i<input.length-1; i++){
+            for(int j=0; j<input.length-1-i; j++){
+                if(input[j+1] < input[j]){
+                    temp=input[j];
+                    input[j] = input[j+1];
+                    input[j+1] = temp;
+                }
+            }
+        }
+        return input;
     }
 }

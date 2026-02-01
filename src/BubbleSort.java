02e5d8bcf8a1c56ef2b4fe00b4303ade59b462ab
JustinHo09 <hojustin09@gmail.com>
2026-01-31 21:46:40 -0600

    I replaced the swap in the body of the if stattment with a method call to an swap method.
    I made a seperate swap method and I use that to swap the values instead of swapping it inside of of if statement itself.

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,12 +5,16 @@ public class BubbleSort implements  SortingAlgorithm{
         for(int i =0; i<input.length-1; i++){
             for(int j=0; j<input.length-1-i; j++){
                 if(input[j+1] < input[j]){
-                    temp=input[j];
-                    input[j] = input[j+1];
-                    input[j+1] = temp;
+                    swap(input, j+1, j);
                 }
             }
         }
         return input;
+    }
+
+    public void swap(int [] input, int index1, int index2){
+        int temp = input[index1];
+        input[index1] = input[index2];
+        input[index2] = temp;
     }
 }

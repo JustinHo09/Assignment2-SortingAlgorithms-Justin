29a7b5934710cbc03293ad11b022ae29fc0eace0
JustinHo09 <hojustin09@gmail.com>
2026-01-31 22:07:56 -0600

    Made a singleTest method, added a random number generator, and loop to add those numbers to an array.
    I added a singleTest method, which will eventually be used to test the algorithm by giving it an array of random numbers, with a specified size in the input. I imported the Random class so we can generate a random number and used a loop to fill in every value of the array. I also added a integer time and return statement for time so that we can return the time it takes for an algorithm to run.

--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,21 @@
+import java.util.Random;
+
 public class Tester {
 
     public Tester (SortingAlgorithm sa){
 
     }
-    
+
+    public double singleTest(int size){
+        int time=0;
+        
+        int[] test = new int[size];
+        Random gen = new Random();
+        for(int i=0; i < size; i++){
+            test[i]= gen.nextInt();
+        }
+
+        return time;
+    }
+
 }

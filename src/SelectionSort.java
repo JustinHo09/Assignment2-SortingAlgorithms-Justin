4b7441a5d8e8e717368c4accbc69dc2dbca73244
JustinHo09 <hojustin09@gmail.com>
2026-01-31 21:52:34 -0600

    Removed temp variable, and swaps with a method call to a swap method.
    I made a swap method that will just swap the two values. I changed the swapping in the for for loop to to a call to the swap method. I also removed the temp variable since it was no longer needed in the sorty method.

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -2,7 +2,6 @@ public class SelectionSort implements SortingAlgorithm {
 
     public int[] sorty(int[] input){
         int smallest;
-        int temp;
         for(int i=0; i<input.length-1; i++){
             smallest=i;
             for(int j=i+1; j<input.length; j++){
@@ -10,10 +9,14 @@ public class SelectionSort implements SortingAlgorithm {
                     smallest=j;
                 }
             }
-            temp = input[i];
-            input[i] = input[smallest];
-            input[smallest] = temp;
+            swap(input,i,smallest);
         }
         return input;
+    }
+
+    public void swap(int [] input, int index1, int index2){
+        int temp = input[index1];
+        input[index1] = input[index2];
+        input[index2] = temp;
     }
 }

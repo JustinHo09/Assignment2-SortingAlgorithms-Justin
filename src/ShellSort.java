050116798f5fe82c3224a6f1e40d47fd498b65bc
JustinHo09 <hojustin09@gmail.com>
2026-01-31 21:36:26 -0600

    Made the loop that will iterate over the gap, and one for checking interleaves, and made a method to sort interleaves.
    I made a for loop that will continually half the gap until it is less than one. Then I made a loop inside of that that will increment until the gap so that it can check each possibe starting position for every interleave. Then I started on another method that will sort the interleaves, that required the integer array, starting position, and gap value as inputs.

--- src/ShellSort.java
+++ src/ShellSort.java
@@ -1,6 +1,18 @@
 public class ShellSort implements SortingAlgorithm{
 
-    int[] sorty(int[] input){
+    public int[] sorty(int[] input){
+
+        for(int gap= input.length/2; gap >=1; gap = gap/2){
+
+            for(int i=0; i<gap;i++){
+                sortInterleave(input,i,gap);
+            }
+        }
+
+        return input;
+    }
+
+    public void sortInterleave(int[] input, int start, int gap){
         
     }
 }

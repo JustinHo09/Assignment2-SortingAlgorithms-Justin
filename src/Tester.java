d409e91f9d0f6d110bdfe58b6ed29b820c66dc59
JustinHo09 <hojustin09@gmail.com>
2026-01-31 22:15:55 -0600

    Added time measurers.
    I added statements to calculate the time it takes an algorithm to run, using the System.nanoTime() methods and subtracted end from start to find the duration. Then I divided that result by one million to convert it into miliseconds. I also left a comment to indicate where the method call to the sorting algorithm's sorty method should be.

--- src/Tester.java
+++ src/Tester.java
@@ -7,13 +7,19 @@ public class Tester {
     }
 
     public double singleTest(int size){
-        int time=0;
-        
+        double time;
+
         int[] test = new int[size];
         Random gen = new Random();
         for(int i=0; i < size; i++){
             test[i]= gen.nextInt();
         }
+
+        long start_time = System.nanoTime();
+        // ADD THE SORTING METHOD CALL HERE
+        long end_time = System.nanoTime();
+
+        time = (end_time - start_time)/1000000;
 
         return time;
     }

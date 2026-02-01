7d160444c8ef0fd3fde6acfacf5fa8aabe4fcaba
JustinHo09 <hojustin09@gmail.com>
2026-01-31 20:59:48 -0600

    I added code for sorty to do selection sort.
    I finished the code for sorty to do selection sort.

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,7 +1,19 @@
 public class SelectionSort implements SortingAlgorithm {
 
     public int[] sorty(int[] input){
-
+        int smallest;
+        int temp;
+        for(int i=0; i<input.length-1; i++){
+            smallest=i;
+            for(int j=i+1; j<input.length; j++){
+                if( input[smallest] > input[j]){
+                    smallest=j;
+                }
+            }
+            temp = input[i];
+            input[i] = input[smallest];
+            input[smallest] = temp;
+        }
         return input;
     }
 }

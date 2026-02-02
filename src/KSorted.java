e31709ae05204bbc9fc3a2ec1717c7cc99efd512
JustinHo09 <hojustin09@gmail.com>
2026-02-01 20:05:55 -0600

    I implemented the generateKSort method and sortInterleaved method.
    I made the generateKSorted method to make na array and then make sure all items are k sorted, in this case since I am using 10 as the gap all of them will be 10 sorted. I use sortInterleave, the same one in shell sort, to make sure everything is k-sorted. So this is implementing one pass through of the outer loop of shell sort.

--- src/KSorted.java
+++ src/KSorted.java
@@ -1,2 +1,29 @@
 public class KSorted {
+
+    public void generateKSorted(int [] my_array){
+        int gap = 10;
+
+        for( int i = 0; i<gap; i++) {
+            sortInterleave(my_array,gap,i);
+        }
+
+    }
+
+    public void sortInterleave(int[] input, int gap, int start){
+        int temp;
+
+        for(int j=start+gap; j<input.length;j=j+gap){
+
+            for(int k=j; k>=gap; k=k-gap){
+
+                if(input[k-gap] > input[k]){
+                    temp = input[k-gap];
+                    input[k-gap] = input[k];
+                    input[k] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
 }

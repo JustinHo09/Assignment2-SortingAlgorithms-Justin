5bc5ee7abd711f376bbaea8a51b38443eafecc51
JustinHo09 <hojustin09@gmail.com>
2026-02-05 07:48:55 -0600

    I made a mergeSort method which will do the recursive splitting calls of mergesort. I also made the header for the merge method to merge the two halves.
    I made a mergeSort method. This method will split the input array in half resursivly and then call merge to put the haves in sorted order. Then after it gets the halves, it will call merge to merge the two lists in sorted order.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,4 +3,30 @@ public class MergeSort implements SortingAlgorithm{
     public int[] sorty(int[] input){
         
     }
+
+    public int[] mergeSort(int [] input){
+        if(input.length>1){
+
+            int mid = input.length/2;
+
+            int[] left = new int[mid];
+            int[] right = new int[input.length-mid];
+
+            for(int i = 0; i<mid; i++){
+                left[i] = input[i];
+            }
+
+            for(int i = mid; i<input.length; i++){
+                right[i] = input[i];
+            }
+            int[] subLeft = mergeSort(left);
+            int[] subRight = mergeSort(right);
+            return merge(subLeft,subRight);
+        }
+        return input;
+    }
+
+    public int[] merge(int[] left, int[] right){
+
+    }
 }

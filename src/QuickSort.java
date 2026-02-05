a29c05ba715a5ae5bc03e82f73a367b99c1c5a92
JustinHo09 <hojustin09@gmail.com>
2026-02-04 20:50:40 -0600

    Added a quicksort method to the QuickSort class.
    I added a method called quickSort to this class, which takes an integer array, integer low and high as its parameter. This will also be implemented in a way that allows recursion. So, I added a base case which detects if the base case is met, which is if the low and high index are the same or high is less than low, and then returns to stop that iteration.

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,7 +1,16 @@
 public class QuickSort implements SortingAlgorithm{
 
     public int[] sorty(int [] input){
-        
+        quickSort(input,0,input.length-1);
+        return input;
     }
+
+    public void quickSort(int [] input, int low, int high){
+        if(high <= low){
+            return;
+        }
+
+    }
+
 
 }

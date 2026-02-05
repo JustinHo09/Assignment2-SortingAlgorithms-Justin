a78150453a957ef75d1dbb58ef0d4d19e3e9329b
JustinHo09 <hojustin09@gmail.com>
2026-02-05 10:13:23 -0600

    I added the merge method to merge the left and right array, added the method call to mergeSort in sorty, and fixed the loop to fill the right array.
    I added the method call to mergeSort in sorty so it should perform merge sort now. Then in mergeSort I fixed an index out of found error by creating a new variable j to be 0 and my index pointer for right, and increment it in the right for loop, which now runs until the end of input is reached and added to j. I then made the merge method which takes in two arrays left and right and makes and returns a new sorted array with the values of the two merged together. This uses a loop to compare values of i and j and adding the smallest each iteration and then. after than if any loop still has values it will add it to the third one.

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,7 +1,7 @@
 public class MergeSort implements SortingAlgorithm{
 
     public int[] sorty(int[] input){
-        
+        return mergeSort(input);
     }
 
     public int[] mergeSort(int [] input){
@@ -15,9 +15,10 @@ public class MergeSort implements SortingAlgorithm{
             for(int i = 0; i<mid; i++){
                 left[i] = input[i];
             }
-
+            int j=0;
             for(int i = mid; i<input.length; i++){
-                right[i] = input[i];
+                right[j] = input[i];
+                j++;
             }
             int[] subLeft = mergeSort(left);
             int[] subRight = mergeSort(right);
@@ -27,6 +28,33 @@ public class MergeSort implements SortingAlgorithm{
     }
 
     public int[] merge(int[] left, int[] right){
-
+        int [] merged = new int[left.length+right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<left.length && j<right.length){
+            if(left[i] < right[j]){
+                merged[k]= left[i];
+                i++;
+            }else{
+                merged[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        if(i< left.length){
+            while(i<left.length){
+                merged[k]=left[i];
+                i++;
+                k++;
+            }
+        }else if(j<right.length){
+            while(j<right.length){
+                merged[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+        return merged;
     }
 }

ddfc2c436029a297d9bd976835735dfaeda790f6
JustinHo09 <hojustin09@gmail.com>
2026-02-04 21:18:35 -0600

    I added a partition method that sorts the partition and returns the index of the pivot.
    I implemented the parition part of quick sort. I used the median of three approach to set the pivot and index equal to the median value. Then I swap the pivot with the last element before I check low and high indexes. Then I check from the low to thenext highest index excluding the last one and swap if the low find a number greater than pivot and high finds a number less than pivot. They then swap and this continues until high and low cross, in which the partition is sorted and then the pivot is swapped with low to get it back in sorted position. I also implemened a swap method that swaps two values position in an array.

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -9,8 +9,63 @@ public class QuickSort implements SortingAlgorithm{
         if(high <= low){
             return;
         }
-
+        int pivot = partition(input,low,high);
     }
+    // [ 0, 5, 7, 9, 10]
+    public int partition(int [] input, int low, int high){
+        int pivot;
+        int pivotIndex;
+        // median of three method to assign a pivot
+        int median = (high+low)/2;
+        if((input[low] <= input[median] && input[median] <= input[high] )||
+                (input[low] >= input[median] && input[median] >= input[high])){
+            pivot = input[median];
+            pivotIndex = median;
+        }else if( (input[median] <= input[low] && input[low] <= input[high]) ||
+                (input[high] <= input[low] && input[low] < input[median])){
+            pivot = input[low];
+            pivotIndex = low;
+        }else{
+            pivot = input[high];
+            pivotIndex = high;
+        }
+        //swap pivot with last element
+        if(pivot != input[high]){
+            swap(input, pivotIndex, high);
+            pivotIndex = high;
+        }
+
+        boolean crossed=false;
+
+        while(!crossed){
+
+            while(input[low] < input[pivot]){
+                low++;
+            }
+
+            while(input[high-1] > input[pivot]){
+                high--;
+            }
+
+            if(high < low){
+                crossed = true;
+                swap(input, pivotIndex, low);
+            }else{
+                swap(input, low, high);
+                low++;
+                high--;
+            }
+        }
+
+        return low;
+    }
+
+    public void swap(int[] input, int first, int second){
+        int temp = input[first];
+        input[first] = input[second];
+        input[second] = temp;
+    }
+
 
 
 }

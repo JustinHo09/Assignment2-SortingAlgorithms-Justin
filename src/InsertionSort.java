553b5e58fa4de2f77cb8a71e1d5b7c52660da36e
JustinHo09 <hojustin09@gmail.com>
2026-02-05 21:16:32 -0600

    I added the swap method to insertion sort to swap.
    I originally had a variable temp in sorty for insertion sort and performed the swap in sorty, but I used swap in the other ones, so I decided to use it here. It is the same swap method the others have, and I call that in the if statement's body instead of a swap in sorty. This is also why I removed the temp variable from sorty since it is not needed anymore.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,20 +1,23 @@
 public class InsertionSort implements SortingAlgorithm{
 
     public int[] sorty(int[] input){
-        int temp;
         for(int i=1; i<input.length;i++){
 
             for(int j=i; j>0; j--){
 
                 if (input[j] < input[j-1]){
-                    temp = input[j-1];
-                    input[j-1] = input[j];
-                    input[j] = temp;
+                    swap(input,j,j-1);
                 }else{
                     break;
                 }
             }
         }
         return input;
+    }
+
+    public void swap(int [] input, int index1, int index2){
+        int temp = input[index1];
+        input[index1] = input[index2];
+        input[index2] = temp;
     }
 }

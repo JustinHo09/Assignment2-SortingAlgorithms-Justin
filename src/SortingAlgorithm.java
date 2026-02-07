f9ab461fdd743bbda6081890e806e0f93cb04184
JustinHo09 <hojustin09@gmail.com>
2026-02-06 21:58:33 -0600

    I added comments.
    I added comments to make it more descriptive.

--- src/SortingAlgorithm.java
+++ src/SortingAlgorithm.java
@@ -1,5 +1,13 @@
+/**
+ * This is an interfcae that will be used to implement the six differnt sorint algorithsm
+ */
 public interface SortingAlgorithm {
 
+    /**
+     * This is the method that will do teh sorting
+     * @param input The array to sort
+     * @return The sorted array.
+     */
     int [] sorty(int[] input);
 
 

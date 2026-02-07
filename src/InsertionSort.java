07e05bf74cc4a97c3068daa03523549d83453d8c
JustinHo09 <hojustin09@gmail.com>
2026-02-06 21:37:55 -0600

    I added comments to insetion sort.
    I added comments to this to make it more descriptive.

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,10 +1,19 @@
+/**
+ * This class implements sorting algorithm to do insertion sort.
+ */
 public class InsertionSort implements SortingAlgorithm{
 
+    /**
+     * This method implement Sorting Algorithm's sorty method to do insertion sort.
+     * @param input The array to sort.
+     * @return The sorted array.
+     */
     public int[] sorty(int[] input){
+        //Starting position
         for(int i=1; i<input.length;i++){
-
+            //Decrements until end is reached or the previous is not bigger than current
             for(int j=i; j>0; j--){
-
+                //if previous is greater than current swap if not exit loop.
                 if (input[j] < input[j-1]){
                     swap(input,j,j-1);
                 }else{
@@ -15,6 +24,12 @@ public class InsertionSort implements SortingAlgorithm{
         return input;
     }
 
+    /**
+     * This method swaps the values of two elements in a list given their index.
+     * @param input List with elements to swap.
+     * @param index1 Index of first element to swap.
+     * @param index2 Index of second element to swap.
+     */
     public void swap(int [] input, int index1, int index2){
         int temp = input[index1];
         input[index1] = input[index2];
